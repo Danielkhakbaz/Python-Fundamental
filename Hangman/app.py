1c740a1abbc4f147c5e1c8161dd8636f028c60fc
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-07-05 16:52:15 +0430

    Hangman Add the duplicated problem in the word

--- Hangman/app.py
+++ Hangman/app.py
@@ -7,6 +7,7 @@ word = []
 choosen_word = random.choice(word_list)
 
 print(logo)
+print(choosen_word)
 
 for _ in range(len(choosen_word)):
     word.append("_")
@@ -15,14 +16,18 @@ while lives != 0:
     if "_" not in word:
         break
 
-    guess = input("Guess a letter: ")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in word:
+        print(f"You've already guessed {guess}.")
 
     for number in range(len(choosen_word)):
         if guess == choosen_word[number]:
             word[number] = guess
     if guess not in choosen_word:
         lives -= 1
-        print(f"You guessed {guess}, That's not in the word.\nYou lose a life.\nNow you have {lives} lives.")
+        print(
+            f"You guessed {guess}, That's not in the word.\nYou lose a life.\nNow you have {lives} lives.")
 
     print(word)
     print(stages[lives])
