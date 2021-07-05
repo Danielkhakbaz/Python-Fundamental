93f29bdc667ab28919aab3ea6d38f52a655b0e88
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-07-05 16:45:32 +0430

    Hangman Add the emojis

--- Hangman/app.py
+++ Hangman/app.py
@@ -6,9 +6,7 @@ lives = 6
 word = []
 choosen_word = random.choice(word_list)
 
-
 print(logo)
-print(choosen_word)
 
 for _ in range(len(choosen_word)):
     word.append("_")
@@ -27,6 +25,7 @@ while lives != 0:
         print(f"You guessed {guess}, That's not in the word.\nYou lose a life.\nNow you have {lives} lives.")
 
     print(word)
+    print(stages[lives])
 
 if lives == 0:
     print("You lost!")
