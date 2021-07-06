7741d69072fc2c2260056f458a5951975c942f91
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-07-06 12:05:58 +0430

    RockPaperScissors Add the game's conditions

--- Rock Paper Scissors/app.py
+++ Rock Paper Scissors/app.py
@@ -10,7 +10,24 @@ def show_the_hand(choose):
     elif choose == 2:
         return scissors
 
-choose = int(input("What do you choose? Type 0 for Rocks, 1 for Paper or 2 for Scissors: "))
+def detect_the_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print("DRAW!")
+    elif computer_choice == 2 and user_choice == 0:
+        print("YOU WIN!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("YOU LOSE!")
+    elif computer_choice > user_choice:
+        print("YOU LOSE!")
+    elif computer_choice < user_choice:
+        print("YOU WIN!")
+    else:
+        print("YOU ENTERED AN INVALID NUMBER!")
 
-print(f"User's choice:\n{show_the_hand(choose)}")
-print(f"Computer's choice:\n{show_the_hand(random.randint(0, 2))}")
+user_choice = int(input("What do you choose? Type 0 for Rocks, 1 for Paper or 2 for Scissors: "))
+computer_choice = random.randint(0, 2)
+
+print(f"User's choice:\n{show_the_hand(user_choice)}")
+print(f"Computer's choice:\n{show_the_hand(computer_choice)}")
+
+detect_the_winner(user_choice, computer_choice)
