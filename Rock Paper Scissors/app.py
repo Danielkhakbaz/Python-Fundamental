e821bb6a50ec70493f5f4b685be3ec8e66a2f8e2
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-07-06 12:09:07 +0430

    RockPaperScissors Add the docstring of the detect_the_winner function

--- Rock Paper Scissors/app.py
+++ Rock Paper Scissors/app.py
@@ -1,6 +1,7 @@
 import random
 from rockPaperScissor_art import rock, paper, scissors
 
+
 def show_the_hand(choose):
     """Return a image due to its condition"""
     if choose == 0:
@@ -10,24 +11,28 @@ def show_the_hand(choose):
     elif choose == 2:
         return scissors
 
-def detect_the_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
-        print("DRAW!")
-    elif computer_choice == 2 and user_choice == 0:
-        print("YOU WIN!")
-    elif computer_choice == 0 and user_choice == 2:
-        print("YOU LOSE!")
-    elif computer_choice > user_choice:
-        print("YOU LOSE!")
-    elif computer_choice < user_choice:
-        print("YOU WIN!")
-    else:
-        print("YOU ENTERED AN INVALID NUMBER!")
 
-user_choice = int(input("What do you choose? Type 0 for Rocks, 1 for Paper or 2 for Scissors: "))
+def detect_the_winner(user_choice, computer_choice):
+    """Return a string for specifying the situation of the game(Draw, Win and Lose)"""
+    if user_choice == computer_choice:
+        return "DRAW!"
+    elif computer_choice == 2 and user_choice == 0:
+        return "YOU WIN!"
+    elif computer_choice == 0 and user_choice == 2:
+        return "YOU LOSE!"
+    elif computer_choice > user_choice:
+        return "YOU LOSE!"
+    elif computer_choice < user_choice:
+        return "YOU WIN!"
+    else:
+        return "YOU ENTERED AN INVALID NUMBER!"
+
+
+user_choice = int(
+    input("What do you choose? Type 0 for Rocks, 1 for Paper or 2 for Scissors: "))
 computer_choice = random.randint(0, 2)
 
 print(f"User's choice:\n{show_the_hand(user_choice)}")
 print(f"Computer's choice:\n{show_the_hand(computer_choice)}")
 
-detect_the_winner(user_choice, computer_choice)
+print(detect_the_winner(user_choice, computer_choice))
