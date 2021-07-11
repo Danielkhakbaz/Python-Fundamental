47cd04bbca7068a8b36b90b9c5ee66f0ce4fb227
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-07-11 12:21:57 +0430

    CaesarCipher Add the whole code to encrypt or decrypt the message based on the user's want

---
+++ Caesar Cipher/app.py
@@ -0,0 +1,31 @@
+from art import logo
+from alphabet import alphabet
+
+again = "yes"
+
+def encrypt_and_decrypt(message, shift_number):
+    """Return a coded string based on the user's want"""
+    code = ""
+
+    for letter in message:
+        code += alphabet[alphabet.index(
+            letter) + shift_number] if choice == "encode" else alphabet[alphabet.index(letter) - shift_number]
+
+    return code
+
+
+print(logo)
+
+while again == "yes" or not "no":
+    choice = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
+
+    if choice != "encode" and choice != "decode":
+        break
+
+    message = input("Type your message: ").lower()
+    shift_number = int(input("Type the shift number: "))
+
+    print(
+        f"Here is the {choice}d result: {encrypt_and_decrypt(message, shift_number)}.")
+
+    again = input("Type 'yes' if you want to go again. Otherwise type 'no': ").lower()
