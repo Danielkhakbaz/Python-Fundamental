aad9f0839f5089793160479e8455f0efd29a290a
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-07-19 19:05:44 +0430

    GuessTheNumber Add the whole game

---
+++ Guess The Number(Computer)/app.py
@@ -0,0 +1,18 @@
+import random
+
+low = 1
+high = int(input("Enter the maximum number inside your mind: "))
+feedback = ""
+
+while feedback != "c":
+    guessed_number = random.randint(low, high)
+
+    feedback = input(
+        f"Is {guessed_number} too high(H), too low(L) or correct(C)? ").lower()
+
+    if feedback == "h":
+        high = guessed_number - 1
+    elif feedback == "l":
+        low = guessed_number + 1
+
+print(f"Yaay, Your number was {guessed_number}.")
