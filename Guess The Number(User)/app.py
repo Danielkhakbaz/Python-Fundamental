3f86060fdfc9e17dd5e71ffdafb3e7d9e8ac09a7
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-07-19 12:22:35 +0430

    GuessTheNumber Remove the function and add few condition instead

--- Guess The Number(User)/app.py
+++ Guess The Number(User)/app.py
@@ -2,13 +2,6 @@ import random
 
 number = random.randint(1, 100)
 
-def check_answer(guess, number):
-    if guess == number: 
-        got_it_right = True
-    else:
-        got_it_right = False
-
-
 print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
 
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
@@ -17,9 +10,17 @@ shots = 10 if difficulty == "easy" else 5
 
 print(number)
 
-print(f"You have {shots} attempts remaining to guess the number.")
-
 for shot in range(1, shots + 1):
+    print(f"You have {shots} attempts remaining to guess the number.")
+
     guess = int(input("Make your guess: "))
 
-    print(check_answer(guess, number))
+    if guess == number:
+        print("GOT IT!")
+        break
+    elif guess > number:
+        print("HIGH")
+    else:
+        print("LOW")
+
+    shots -= 1
