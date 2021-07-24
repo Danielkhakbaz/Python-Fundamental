caf6997d98ff9aacc9239d7f82fbafd41ea3fed4
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-07-24 10:31:42 +0430

    Countdown Complete the getting data from the user part

---
+++ Countdown/app.py
@@ -0,0 +1,7 @@
+goal_with_date = input(
+    "Enter your goal with a deadline separated by a comma(EX: Learn Python, 20.06.2021): ")
+
+goal, date = goal_with_date.split(", ")
+
+print(goal)
+print(date)
