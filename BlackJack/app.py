38ac1071b348a3471b34f5bda28ba6db37def6f6
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-07-24 10:36:59 +0430

    Blackjack Refactor the code(Change the variable's constant name to capital case

--- BlackJack/app.py
+++ BlackJack/app.py
@@ -1,9 +1,9 @@
 import random
 from art import logo
 
+CARDS = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
 choice = "y"
 game_is_over = False
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
 user_cards = []
 dealer_cards = []
 
@@ -19,12 +19,12 @@ def game_detector():
 
 
 for _ in range(2):
-    user_cards.append(random.choice(cards))
-    dealer_cards.append(random.choice(cards))
+    user_cards.append(random.choice(CARDS))
+    dealer_cards.append(random.choice(CARDS))
 
 print(logo)
 print(f"Your cards: {user_cards} -> {sum(user_cards)}")
-print(f"Dealer's first card: {dealer_cards[0]}")
+print(f"Dealer's first card: {dealer_cards.get[0]}")
 
 while (not game_is_over) and (choice == "y"):
     if sum(user_cards) == 21:
@@ -35,7 +35,7 @@ while (not game_is_over) and (choice == "y"):
         "Type 'y' to get another card, type 'n' to pass on it: ").lower()
 
     if choice == "y":
-        user_cards.append(random.choice(cards))
+        user_cards.append(random.choice(CARDS))
 
         print(f"Your cards: {user_cards} -> {sum(user_cards)}")
 
@@ -47,7 +47,7 @@ while (not game_is_over) and (choice == "y"):
             break
     else:
         while sum(dealer_cards) < 17:
-            dealer_cards.append(random.choice(cards))
+            dealer_cards.append(random.choice(CARDS))
 
         print(f"Dealer's cards: {dealer_cards}")
 
