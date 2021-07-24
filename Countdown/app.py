7fb3748fe8e42e5d3528354b9275ffe6159e9281
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-07-24 14:59:59 +0430

    Countdown Add the whole countdown app

--- Countdown/app.py
+++ Countdown/app.py
@@ -1,7 +1,15 @@
+import datetime
+
 goal_with_date = input(
     "Enter your goal with a deadline separated by a comma(EX: Learn Python, 20.06.2021): ")
 
 goal, date = goal_with_date.split(", ")
+[day, month, year] = map(int, date.split("."))
 
-print(goal)
-print(date)
+today = datetime.date.today()
+someday = datetime.date(year, month, day)
+
+diffrence = today - someday
+
+print(
+    f"Dear user! Time remaining for your goal: {goal} is {diffrence.days} days.")
