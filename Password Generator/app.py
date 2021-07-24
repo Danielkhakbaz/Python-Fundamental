bffdb2eb2c809afd8cf58c19f80e6441a0821c6c
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-07-24 11:02:27 +0430

    PasswordGenerator Refactor the code(Change the variables constant names in capital case)

--- Password Generator/app.py
+++ Password Generator/app.py
@@ -1,12 +1,8 @@
 import random
+from password_characters import WORDS, NUMBERS, SYMBOLS
 
 password_list = []
 password = ""
-
-words = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y",
-         "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
 
 print("Welcome to the PyPassword Generator!")
 words_number = int(
@@ -16,11 +12,11 @@ symbols_number = int(input("How many symbols would you like? "))
 
 if numbers_number + symbols_number <= words_number:
     for word in range(abs(words_number - (symbols_number + numbers_number))):
-        password_list += random.choice(words)
+        password_list += random.choice(WORDS)
     for number in range(numbers_number):
-        password_list += random.choice(numbers)
+        password_list += random.choice(NUMBERS)
     for symbol in range(symbols_number):
-        password_list += random.choice(symbols)
+        password_list += random.choice(SYMBOLS)
 
     random.shuffle(password_list)
 
