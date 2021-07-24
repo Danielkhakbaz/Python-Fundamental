df28f9c1ae3b0391e5342c89655075c786947da8
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-07-24 11:05:17 +0430

    RockPaperScissors Refactor the code(Change the variables constant names in capital case and add paranthesis in if statements)

--- Rock Paper Scissors/app.py
+++ Rock Paper Scissors/app.py
@@ -1,24 +1,24 @@
 import random
-from rockPaperScissor_art import rock, paper, scissors
+from rockPaperScissor_art import ROCK, PAPER, SCISSORS
 
 
 def show_the_hand(choose):
     """Return a image due to its condition"""
     if choose == 0:
-        return rock
+        return ROCK
     elif choose == 1:
-        return paper
+        return PAPER
     elif choose == 2:
-        return scissors
+        return SCISSORS
 
 
 def detect_the_winner(user_choice, computer_choice):
     """Return a string for specifying the situation of the game(Draw, Win and Lose)"""
     if user_choice == computer_choice:
         return "DRAW!"
-    elif computer_choice == 2 and user_choice == 0:
+    elif (computer_choice == 2) and (user_choice == 0):
         return "YOU WIN!"
-    elif computer_choice == 0 and user_choice == 2:
+    elif computer_choice == 0 and (user_choice == 2):
         return "YOU LOSE!"
     elif computer_choice > user_choice:
         return "YOU LOSE!"
@@ -29,7 +29,7 @@ def detect_the_winner(user_choice, computer_choice):
 
 
 user_choice = int(
-    input("What do you choose? Type 0 for Rocks, 1 for Paper or 2 for Scissors: "))
+    input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
 computer_choice = random.randint(0, 2)
 
 print(f"User's choice:\n{show_the_hand(user_choice)}")
