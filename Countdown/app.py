7921ccf02556e66ea6779315f7f6a35d68039ac6
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-08-23 19:28:59 +0430

    Countdown Add the map function for getting goal and data

--- Countdown/app.py
+++ Countdown/app.py
@@ -3,7 +3,7 @@ import datetime
 goal_with_date = input(
     "Enter your goal with a deadline separated by a comma(EX: Learn Python, 20.06.2021): ")
 
-goal, date = goal_with_date.split(", ")
+[goal, date] = map(str, goal_with_date.split(", "))
 [day, month, year] = map(int, date.split("."))
 
 someday = datetime.date(year, month, day)
