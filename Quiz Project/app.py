334ab46674f5feadfe2a5248bfd2065f40a35f3d
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-09-27 12:22:56 +0330

    QuizProject Delete the time module and the code related to it

--- Quiz Project/app.py
+++ Quiz Project/app.py
@@ -1,10 +1,6 @@
+from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
-from data import question_data
-
-import time
-
-start_time = time.time()
 
 question_bank = []
 
@@ -16,8 +12,6 @@ qb = QuizBrain(question_bank)
 while qb.still_has_questions():
     print(qb.next_question())
 
-print("- + - + - + - + - + - + - + - + - + ")
+print("+- + - + - + - + - + - + - + - + - + ")
 print("You've completed the Quiz!")
 print(f"Your final score was: {qb.correct_answers}/{len(question_bank)}")
-
-print("--- %s seconds ---" % (time.time() - start_time))
